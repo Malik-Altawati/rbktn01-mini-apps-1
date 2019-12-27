b543bde7c02ac496861abc2d6c31effb85e09037
MalikAltawati <Malik.abdullah.abraheem@gmail.com>
2019-12-27 19:51:55 +0100

    refactored to become dry

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,11 +1,23 @@
-
 $(document).ready(function () {
-  $(".board").html("-")
   var turn = 0
   var array = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-  var first = prompt("Enter first player name : ")
-  var second = prompt("Enter second player name : ")
-
+  ////
+  while (!first) {
+    var first = prompt("Enter first player name : ")
+  };
+  while (!second) {
+    var second = prompt("Enter second player name : ")
+  };
+  //
+  $("#player1").text(first)
+  $("#vs").text(" VS ")
+  $("#player2").text(second)
+  //
+  var reset = function () {
+    $(".board").html("-")
+    array = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+    turn = 0
+  }
   $(".board").on("click", function (event) {
     if (turn === 8) {
       alert("its a tie")
@@ -35,112 +47,50 @@ $(document).ready(function () {
         if (theID === 6 || theID === 7 || theID === 8) {
           array[2][theID - 6] = "O"
         }
-
       }
       //////////////////////////////////////////////////////////////////////////////////checking part
       //winner condition here for //// Major diagonal
-      var test = 0
-      var counter = 0
-      for (var i = 0; i < array.length; i++) {
-        if (array[i][test] === "X") {
-          counter++
-          if (counter === 3) { alert("The Winner is : " + first) }
-        }
-        test++
+      if (array[0][0] === "X" & array[1][1] === "X" & array[2][2] === "X") {
+        alert("The Winner is : " + first)
       }
-
-      test = 0;
-      counter = 0
-      for (var i = 0; i < array.length; i++) {
-        if (array[i][test] === "O") {
-          counter++
-          if (counter === 3) { alert("The Winner is : " + second) }
-        }
-        test++
+      if (array[0][0] === "O" & array[1][1] === "O" & array[2][2] === "O") {
+        alert("The Winner is : " + second)
       }
       //winner condition here for //// Minor diagonal
-      test = 2
-      counter = 0
-      for (var i = 0; i < array.length; i++) {
-        if (array[i][test] === "X") {
-          counter++
-          if (counter === 3) { alert("The Winner is : " + first) }
-        }
-        test--
+      if (array[0][2] === "X" & array[1][1] === "X" & array[2][0] === "X") {
+        alert("The Winner is : " + first)
       }
-
-      test = 2
-      counter = 0
-      for (var i = 0; i < array.length; i++) {
-        if (array[i][test] === "O") {
-          counter++
-          if (counter === 3) { alert("The Winner is : " + second) }
-        }
-        test--
+      if (array[0][2] === "O" & array[1][1] === "O" & array[2][0] === "O") {
+        alert("The Winner is : " + second)
       }
-
       //winner condition here for //// horizantal
-
       for (var i = 0; i < array.length; i++) {
-        counter = 0
-        for (var j = 0; j < array.length; j++) {
-          if (array[i][j] === "X") {
-            counter++
-            if (counter === 3) { alert("The Winner is : " + first) }
-          }
+        if (array[i][0] === "X" & array[i][1] === "X" & array[i][2] === "X") {
+          alert("The Winner is : " + first)
         }
-        counter = 0
-        for (var k = 0; k < array.length; k++) {
-          if (array[i][k] === "O") {
-            counter++
-            if (counter === 3) { alert("The Winner is : " + second) }
-          }
+        if (array[i][0] === "O" & array[i][1] === "O" & array[i][2] === "O") {
+          alert("The Winner is : " + second)
         }
       }
-
       //winner condition here for //// vertical
-      var tester = 0
-      counter = 0
-      for (var i = 0; i < array.length; i++) {
-        for (var j = 0; j < array.length; j++) {
-          if (array[j][tester] === "X") {
-            counter++
-            if (counter === 3) { alert("The Winner is : " + first) }
-          }
-        }
-        counter = 0
-        tester++
+      if (array[0][0] === "X" & array[1][0] === "X" & array[2][0] === "X" ||
+        array[0][1] === "X" & array[1][1] === "X" & array[2][1] === "X" ||
+        array[0][2] === "X" & array[1][2] === "X" & array[2][2] === "X"
+      ) {
+        alert("The Winner is : " + first)
       }
-      //
-
-      var tester = 0
-      counter = 0
-      for (var i = 0; i < array.length; i++) {
-        for (var j = 0; j < array.length; j++) {
-          if (array[j][tester] === "O") {
-            counter++
-            if (counter === 3) { alert("The Winner is : " + second) }
-          }
-        }
-        counter = 0
-        tester++
+      if (array[0][0] === "O" & array[1][0] === "O" & array[2][0] === "O" ||
+        array[0][1] === "O" & array[1][1] === "O" & array[2][1] === "O" ||
+        array[0][2] === "O" & array[1][2] === "O" & array[2][2] === "O"
+      ) {
+        alert("The Winner is : " + second)
       }
-
-
-      //////////////////////////////
-      console.log(array)
       turn++
     }
-
     $("#reset").on("click", function () {
-      $(".board").html("-")
-      array = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-      turn = 0
+      reset()
     })
-
   })
-
-
 });
 
 
