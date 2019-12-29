093134d3c8504e81cb23dfad6bb59eb62cecf3c4
MalikAltawati <Malik.abdullah.abraheem@gmail.com>
2019-12-29 20:29:42 +0100

    converter and template are done

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,6 +1,7 @@
 
 const express = require("express");
 const bodyParser = require('body-parser')
+const fs = require('fs')
 const PORT = 1234;
 const app = express();
 app.use(express.static('client'))
@@ -9,18 +10,50 @@ app.use(bodyParser.json())
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.post('/', urlencodedParser, function (req, res) {
-    data = req.body.JSON
-    var x = jsonToCsv(JSON.parse(data))
-    res.end("<h1>SCV FORMAT HERE </h1>\n" + x)
+    data = JSON.parse(req.body.JSON)
+    var ready = convertor(data)
+    // console.log(ready)
+    fs.writeFile('test.csv', ready, (err, data) => {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log('created')
+        }
+    })
+    res.end(temp(ready))
 })
 
 app.listen(1234, () => {
     console.log(`server is listening on port: ${PORT}`)
 })
 
-var jsonToCsv = (obj) => {
-    const header = 'firstName,lastName,county,city,role,sales';
-    const headerArray = header.split(',');
-    return headerArray
-};
+var convertor = function (data) {
+    arr = []
+    var vars = function (Dayta) {
+        var elem = {}
+        for (var key in Dayta) {
+            if (key != "children") {
+                elem[key] = Dayta[key]
+            }
+        }
+        arr.push(elem)
+        for (var i = 0; i < Dayta["children"].length; i++) {
+            vars(Dayta["children"][i])
+        }
+    }
+    vars(data)
+    var vals = [];
+    var keys = Object.keys(arr[0])
+    for (var i = 0; i < arr.length; i++) {
+        vals.push(Object.values(arr[i]))
+    }
+    var result = keys.join(",") + "\n"
+    for (var i = 0; i < vals.length; i++) {
+        result += vals[i].join(",") + "\n"
+    }
+    return result
+}
 
+var temp = function (val) {
+    return `<h1>SCV FORMAT HERE </h1><div>${val}</div>`
+}
